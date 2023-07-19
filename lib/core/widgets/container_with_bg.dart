fa105d17711d4f0ed2032963675f3cfd3dbb40b1
khursheed33 <khursheed.gaddi@e-zest.in>
2023-07-19 19:52:35 +0530

    common widgets improved

---
+++ lib/core/widgets/container_with_bg.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class AppContainerWithBG extends StatelessWidget {
+  final Widget child;
+  final Color? color;
+  final double? width;
+  final double? maxWidth;
+  final double? radius;
+  final double? maxHeight;
+  final Color? borderColor;
+  final double? borderWidth;
+  final double? marginVertical;
+  final double? marginHorizontal;
+  final List<BoxShadow>? boxShadow;
+  final EdgeInsetsGeometry? padding;
+  final EdgeInsetsGeometry? margin;
+  const AppContainerWithBG({
+    Key? key,
+    required this.child,
+    this.color,
+    this.radius,
+    this.width,
+    this.margin,
+    this.padding,
+    this.boxShadow,
+    this.borderColor,
+    this.borderWidth,
+    this.marginVertical,
+    this.marginHorizontal,
+    this.maxWidth,
+    this.maxHeight,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width ?? MediaQuery.of(context).size.width,
+      padding: padding ?? const EdgeInsets.all(10),
+      margin: margin,
+      decoration: BoxDecoration(
+        color: color ?? Theme.of(context).colorScheme.background,
+        borderRadius: BorderRadius.circular(radius ?? 10),
+        boxShadow: boxShadow,
+        border: Border.all(
+          width: borderWidth ?? 0.0,
+          color: borderColor ?? Theme.of(context).colorScheme.background,
+        ),
+      ),
+      child: child,
+    );
+  }
+}
